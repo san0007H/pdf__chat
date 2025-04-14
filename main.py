b9f3b6d45b595cedfba1ba9ed706bfd5b6efd3d3
san0007s <149048722+san0007s@users.noreply.github.com>
2025-04-14 10:47:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
